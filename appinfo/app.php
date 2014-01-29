4347a6ac0f3dc0d22bc4f9395515fabc729fedd8
awikatchikaen <lord.awikatchikaen@gmail.com>
2014-01-29 21:04:04 +0100

    initial release

---
+++ appinfo/app.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* @package menu_on_top an ownCloud app
+* @category base
+* @author Awikatchikaen
+* @copyright 2012-2013 Awikatchikaen <lord.awikatchikaen@gmail.com>
+* @license GNU Affero General Public license (AGPL)
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the license, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.
+* If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+/**
+ * @file appinfo/app.php
+
+ * @author Awikatchikaen
+ */
+
+OCP\Util::addScript ( 'menu_on_top', 'menu_on_top' );
+OCP\Util::addStyle  ( 'menu_on_top', 'menu_on_top' );
+
+?>
---
+++ js/menu_on_top.js
@@ -0,0 +1,62 @@
+/**
+* @package menu_on_top
+* @category base
+* @author Awikatchikaen
+* @copyright 2012-2013 Awikatchikaen <lord.awikatchikaen@gmail.com>
+* @license GNU Affero General Public license (AGPL)
+* @link information http://apps.owncloud.com/content/show.php?content=157091
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the license, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.
+* If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+/**
+ * @file js/menu_on_top.JS
+ * @author Awikatchikaen
+ */
+
+
+
+$(document).ready(function () {
+  $("#navigation").hide();
+  $("#navigation").css("visibility", "visible");
+  $("#owncloud").click(function(e){
+    e.preventDefault();
+    $("#navigation").slideToggle(); 
+  });
+  
+  
+  $("#navigation li a").click(function(e){
+    $("#navigation").slideUp(); 
+  });
+
+  /*$("#navigation, #owncloud").mouseleave(function(){
+    $("#navigation").slideUp(); 
+  });*/
+});
+
+
+/*
+$(function () {
+    //$(".campaign-box span:last").hide();
+    $("#owncloud").hover(function () { 
+        //$("#navigation").css("opacity", 0.5);
+        $("#navigation").fadeIn("fast");           
+    }, function () {        
+        //$("#navigation").css("opacity", 1); 
+        $("#navigation").hide("slow");
+    });
+});
+*/
